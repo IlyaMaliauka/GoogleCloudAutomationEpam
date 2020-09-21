4d4cee12679290c47533d2617eed9a35e7d5f176
IlyaMaliauka <molyavko.edu@gmail.com>
2020-09-21 16:55:27 +0300

    fix

--- src/test/java/com/epam/automation/page/FakeMailHomePage.java
+++ src/test/java/com/epam/automation/page/FakeMailHomePage.java
@@ -58,6 +58,7 @@ public class FakeMailHomePage {
 
         ArrayList<String> tabs2 = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(1));
+        driver.navigate().refresh();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
